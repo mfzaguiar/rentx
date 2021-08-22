1a972508bad6327afbc398ba511c4ee2fe965ee7
Matheus Aguiar <first_mf@hotmail.com>
2021-08-22 16:21:59 -0300

    add: Scheduling screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+
+import { Button } from '../../components/Button';
+import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateWrapper,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+
+export function Scheduling() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateWrapper selected={false}>
+              <DateValue>18/06/2021</DateValue>
+            </DateWrapper>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATE</DateTitle>
+            <DateWrapper selected={true}>
+              <DateValue>18/06/2021</DateValue>
+            </DateWrapper>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="Continuar" />
+      </Footer>
+    </Container>
+  );
+}
